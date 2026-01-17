89d52632a0d4e7703ae644f79946f1647eece59a
Hamida Badamdi <hamida.badamdi143121@marwadiuniversity.ac.in>
2026-01-17 22:21:32 +0530

    Update queue_using_array.c

--- queue_using_array.c
+++ queue_using_array.c
@@ -1,127 +1,122 @@
-#include<stdio.h>
-#define MAX 5
+#include <stdio.h>
+#define MAX 5   // Maximum size of the queue
 
-int front = -1 , rear = -1;
-int queue[MAX];
+int front = -1, rear = -1;   // Queue pointers
+int queue[MAX];              // Queue array
 
+// Function declarations
 void insert();
 void delete_element();
 void display();
 
-void main()
+int main()
 {
     int op;
 
+    // Menu-driven program
     do
     {
+        printf("\n\n---- Queue Operations ----");
         printf("\n1. Insert");
         printf("\n2. Delete");
         printf("\n3. Display");
         printf("\n4. Exit");
 
         printf("\nEnter Your Choice : ");
-        scanf("%d" , &op);
+        scanf("%d", &op);
 
-
-        switch(op)
+        switch (op)
         {
             case 1:
-                insert();
+                insert();          // Enqueue operation
                 break;
             case 2:
-                delete_element();
+                delete_element();  // Dequeue operation
                 break;
             case 3:
-                display();
+                display();         // Display queue elements
                 break;
             case 4:
-                printf("\nExit...");
+                printf("\nExiting program...");
                 break;
             default:
-                printf("\nInvalid Option !");
-                break;
-
-
-
+                printf("\nInvalid Option!");
         }
 
-    }while(op!=4);
+    } while (op != 4);
 
+    return 0;
 }
+
+// Function to insert an element into the queue
 void insert()
 {
     int val;
-    if(rear == MAX -1 )
+
+    // Check for queue overflow
+    if (rear == MAX - 1)
     {
-        printf("\nQueue Overflow !");
+        printf("\nQueue Overflow!");
         return;
     }
 
+    printf("\nEnter value to be inserted : ");
+    scanf("%d", &val);
 
+    // If queue is empty, initialize front and rear
+    if (front == -1 && rear == -1)
+    {
+        front = rear = 0;
+    }
     else
     {
-        printf("\n\nEnter Value to be inserted : ");
-        scanf("%d" , &val);
-
-        if(front == -1 && rear == -1)
-        {
-
-            front = rear = 0;
-        }
-        else
-        {
-
-            rear++;
-        }
-
-        queue[rear] = val;
+        rear++;   // Move rear forward
     }
 
+    queue[rear] = val;  // Insert element
 }
+
+// Function to delete an element from the queue
 void delete_element()
 {
     int val;
 
-    if(front == -1)
+    // Check for queue underflow
+    if (front == -1)
     {
-
-        printf("\n Queue is empty!");
+        printf("\nQueue is empty!");
         return;
     }
+
+    val = queue[front];
+    printf("\n%d is deleted...", val);
+
+    // If only one element was present
+    if (front == rear)
+    {
+        front = rear = -1;  // Reset queue
+    }
     else
     {
-        val = queue[front];
-        printf("%d is deleted ..." , val);
-
-        if(front == rear)
-        {
-            front = rear = -1;
-        }
-        else{
-            front++;
-        }
+        front++;  // Move front forward
     }
-
-
 }
+
+// Function to display queue elements
 void display()
 {
-        if(front != -1)
-        {
-            printf("\n Elements are...");
+    int i;
 
-            for(int i=front; i<=rear; i++)
-            {
-
-                printf("\t%d"  ,queue[i]);
-            }
-
-       }
-    else
+    // Check if queue is empty
+    if (front == -1)
     {
-
-        printf("\n queue is empty!");
+        printf("\nQueue is empty!");
+        return;
     }
 
+    printf("\nQueue elements are:");
+    for (i = front; i <= rear; i++)
+    {
+        printf("\t%d", queue[i]);
+    }
 }
-
